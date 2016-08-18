eca0ad16589116acf769c07fcccb96a499229f16
Elysia Nason and Satchel Grant <elysia@>
2016-08-18 10:21:13 -0700

    add functionality to Numeralize function

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,28 @@
-function pigLatinMaker(wordIn, index) {
-  var puncts = /[.,?!:';]$/;
-  if (puncts.test(wordIn)) {
-    return wordIn.slice(index, -1) + wordIn.slice(0,index) + "ay" + wordIn.charAt(wordIn.length-1);
-  }
-  else {
-    return wordIn.slice(index) + wordIn.slice(0,index) + "ay";
-  }
-}
-function pigLatinTranslator(input) {
-  var words = input.split(/[\s]+/);
-  var vowels = /^[aeiou]/;
-  words.forEach(function(word, elementIndex) {
-    var isVowel = false;
-    var vowelIndex = 0;
-    isVowel = vowels.test(word);
-    if (isVowel) {
-      words[elementIndex] = pigLatinMaker(word, vowelIndex);
+//BUSINESS LOGIC
+function Numeralize(numberIn) {
+  var ones = ["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  var tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  var hundos = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  var thousands = ["", "M", "MM", "MMM"];
+  var numOneIn = parseInt(numberIn.slice(-1));
+  var numTensIn = parseInt(numberIn.slice(-2, -1));
+  var numHundosIn = parseInt(numberIn.slice(-3, -2));
+  var numThousandsIn = parseInt(numberIn.slice(-4, -3));
+  var numArray = [numOneIn, numTensIn, numHundosIn, numThousandsIn];
+  for (index=0; index<numArray.length; index+=1) {
+  	if (!numArray[index]) {
+    	numArray[index]=0;
     }
-    else {
-      for (var i = 0; i < word.length; i++) {
-        if(vowelIndex == 0 && word[i] === "q") {
-          vowelIndex = i+2;
-        }
-        else if (vowelIndex == 0 && vowels.test(word[i])) {
-          vowelIndex = i;
-        }
-      }
-      words[elementIndex] = pigLatinMaker(word, vowelIndex);
-    }
-  });
-  return words.join(" ");
+  }
+  return thousands[numArray[3]]+hundos[numArray[2]]+tens[numArray[1]]+ones[numArray[0]];
 }
-$(function() {
-  $("form").submit(function(event) {
-      event.preventDefault();
-      var inputText = $("textarea").val();
-      var output = pigLatinTranslator(inputText);
-      $("#output").text(output);
+
+//UI LOGIC
+$(document).ready(function(){
+  $("form").submit(function(event){
+    var userInput = $("input#natural-number").val();
+    var result = Numeralize(userInput);
+    $("#output").text(result);
+    event.preventDefault();
   });
 });
